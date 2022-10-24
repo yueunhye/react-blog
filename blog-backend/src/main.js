1ccd703848348d9752dd5b2e8459d6eaa0ad1210
유은혜 <yueunhye@yueunhyeui-MacBookPro.local>
2022-10-25 01:55:31 +0900

    feat: Add Koa-static

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,21 +3,23 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import mongoose from 'mongoose'
+import serve from 'koa-static'
+import path from 'path'
+import send from 'koa-send'
 
 import api from './api'
 import jwtMiddleware from './lib/jwtMiddleware'
 
-
 const { PORT, MONGO_URL } = process.env
 
-
-mongoose.connect(MONGO_URL)
-	.then(()=>{
-		console.log('Connect to MongoDB')
-	})
-	.catch((e)=>{
-		console.error(e)
-	})
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log('Connect to MongoDB')
+  })
+  .catch(e => {
+    console.error(e)
+  })
 
 const app = new Koa()
 const router = new Router()
@@ -26,9 +28,17 @@ router.use('/api', api.routes())
 
 app.use(bodyParser()) // 라우터 적용 전 bodyParser 적용
 app.use(jwtMiddleware)
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods())
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build')
+app.use(serve(buildDirectory))
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory })
+  }
+})
 
 const port = PORT || 4000
 app.listen(port, () => {
-	console.log('Listening to port %d', port)
+  console.log('Listening to port %d', port)
 })
